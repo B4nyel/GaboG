c26ea114f75499b02762256ecc2a6ac02b2d6657
Danielius194 <rohondaniel@gmail.com>
2023-12-06 15:25:04 +0100

    rakosnik

---
+++ commands/misc/miny.js
@@ -0,0 +1,33 @@
+const { ApplicationCommandOptionType, PermissionFlagsBits} = require('discord.js');
+const { Minesweeper } = require('discord-gamecord');
+
+module.exports = {
+    deleted: false,
+    name: 'miny',
+    description: 'Zahraj si míny!',
+
+    callback: (client, interaction) => {
+
+        const Game = new Minesweeper({
+            message: interaction,
+            isSlashGame: true,
+            embed: {
+              title: 'Minesweeper',
+              color: '#5865F2',
+              description: 'Click on the buttons to reveal the blocks except mines.'
+            },
+            emojis: { flag: '🚩', mine: '💣' },
+            mines: 5,
+            timeoutTime: 60000,
+            winMessage: 'You won the Game! You successfully avoided all the mines.',
+            loseMessage: 'You lost the Game! Beaware of the mines next time.',
+            playerOnlyMessage: 'Only {player} can use these buttons.'
+          });
+          
+          Game.startGame();
+          Game.on('gameOver', result => {
+            console.log(result);  // =>  { result... }
+          });
+    },
+  };
+  
